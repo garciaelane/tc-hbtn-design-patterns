4a0894e8c1d975b3d8c307d04d154d15f129f643
Elane Garcia <elanegarcia@ciandt.com>
2022-08-18 17:23:44 -0300

    Task 0

--- 0x00/personagem/PersonagemFactory.java
+++ 0x00/personagem/PersonagemFactory.java
@@ -1,6 +1,6 @@
 public abstract class PersonagemFactory {
 
-    public abstract Personagem createPersonagem(TipoPersonagem tipo, String nome) throws Exception;
+    public abstract personagem.Personagem createPersonagem(TipoPersonagem tipo, String nome) throws Exception;
 
 
 }
