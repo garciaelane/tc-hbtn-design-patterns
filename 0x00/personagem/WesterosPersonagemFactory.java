1d4b55edb8d430a3e19867003ab6dfe51ef34b58
Elane Garcia <elanegarcia@ciandt.com>
2022-08-22 17:20:53 -0300

    Task 0

--- 0x00/personagem/WesterosPersonagemFactory.java
+++ 0x00/personagem/WesterosPersonagemFactory.java
@@ -1,13 +1,14 @@
 public class WesterosPersonagemFactory extends PersonagemFactory {
 
-
     @Override
     public Personagem createPersonagem(TipoPersonagem tipo, String nome) {
-        if (tipo == TipoPersonagem.MAGO) {
+        else if (tipo == TipoPersonagem.MAGO) {
             return new Mago(nome, 8, 3, 3, 3, 4);
-        } else if (tipo == TipoPersonagem.LADRAO) {
+        }
+        else if (tipo == TipoPersonagem.LADRAO) {
             return new Ladrao(nome, 1, 8, 7, 7, 10);
-        } else if (tipo == TipoPersonagem.GUERREIRO) {
+        }
+        else if (tipo == TipoPersonagem.GUERREIRO) {
             return new Guerreiro(nome, 0, 9, 7, 10, 7);
         }
         return null;
